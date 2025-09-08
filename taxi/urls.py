a515c4b1f31d9c141eb26145c1b610419778b265
filip <filip1folga@gmail.com>
2025-09-08 15:06:24 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 
+from taxi.views import (
+    ManufacturerListView,
+    CarListView,
+    CarDetailView,
+    DriverListView,
+    DriverDetailView,
+)
+
 from .views import index
 
 urlpatterns = [
@@ -7,3 +15,15 @@ urlpatterns = [
 ]
 
 app_name = "taxi"
+
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("manufacturers/", ManufacturerListView.as_view(),
+         name="manufacturer-list"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(),
+         name="driver-detail"),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(),
+         name="car-detail"), ]
--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from django.views import generic
 from taxi.models import Driver, Car, Manufacturer
 
 
@@ -13,3 +14,34 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    """View function for the home page of the site."""
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    """View function for the home page of the site."""
+    model = Car
+    queryset = Car.objects.select_related("manufacturer").all()
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    """View function for the home page of the site."""
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    """View function for the home page of the site."""
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    """View function for the home page of the site."""
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
