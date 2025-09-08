5dc3dc51ce03c16739267ea07d7c8697f4cc1297
filip <filip1folga@gmail.com>
2025-09-08 15:20:17 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -5,7 +5,6 @@ from taxi.models import Driver, Car, Manufacturer
 
 
 def index(request):
-    """View function for the home page of the site."""
 
     context = {
         "num_drivers": Driver.objects.count(),
@@ -17,31 +16,31 @@ def index(request):
 
 
 class ManufacturerListView(generic.ListView):
-    """View function for the home page of the site."""
+
     model = Manufacturer
     queryset = Manufacturer.objects.all().order_by("name")
     paginate_by = 5
 
 
 class CarListView(generic.ListView):
-    """View function for the home page of the site."""
+
     model = Car
     queryset = Car.objects.select_related("manufacturer").all()
     paginate_by = 5
 
 
 class CarDetailView(generic.DetailView):
-    """View function for the home page of the site."""
+
     model = Car
 
 
 class DriverListView(generic.ListView):
-    """View function for the home page of the site."""
+
     model = Driver
     paginate_by = 5
 
 
 class DriverDetailView(generic.DetailView):
-    """View function for the home page of the site."""
+
     model = Driver
     queryset = Driver.objects.prefetch_related("cars__manufacturer")
